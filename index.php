4f97f5948d7a957dcc794f392d8eb326e5c2a156
Brenno <brennohenriqueskt@gmail.com>
2018-05-07 15:50:39 -0300

    Nova alteração de desenvolvimento

--- index.php
+++ index.php
@@ -16,60 +16,72 @@
 
   <body>
 
-    <main class="container-fluid principal">
-      <header class="row">
-        <nav class="navbar container-fluid">
-            <a href="#" class="navbar-brand">Logo</a>
-            <ul class="nav">
-              <li class="dropdown cat">
+    <main class="principal">
+      <header class="cabecalho">
+        <nav class="navbar justify-content-arround">
+            <a href="#" class="nav-item logo">
+                <img src="_img/logo.jpg" class="img-fluid"/>
+            </a>
+            <ul class="nav-item nav navi">
+              <li class="nav-item dropdown cat">
                 <a href="#" class="nav-link" id="linkDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <i class="fas fa-th"></i>Categorias
                 </a>
                 <div class="dropdown-menu cat2" aria-labelledby="linkDrop">
-                  <table>
-                    <tr>
-                      <td><a href="dropdown-item">Hortifruti</a></td><td><a href="dropdown-item">Açougue</a></td>
-                    </tr>
-                    <tr>
-                      <td><a href="dropdown-item">Frios e Laticínios</a></td><td><a href="dropdown-item">Adega</a></td>
-                    </tr>
-                    <tr>
-                      <td><a href="dropdown-item">Papelaria</a></td><td><a href="dropdown-item">Padaria e Confeitaria</a></td>
-                    </tr>
-                    <tr>
-                      <td><a href="dropdown-item">Produtos para seu Animal</a></td><td><a href="dropdown-item">Produtos de Limpeza</a></td>
-                    </tr>
-                    <tr>
-                      <td><a href="dropdown-item">Para sua Casa</a></td>
-                    </tr>
-                  </table>
+                  <a href="dropdown-item">Hortifruti</a><br/>
+                  <a href="dropdown-item">Açougue</a><br/>
+                  <a href="dropdown-item">Frios e Laticínios</a><br/>
+                  <a href="dropdown-item">Adega</a><br/>
+                  <a href="dropdown-item">Papelaria</a><br/>
+                  <a href="dropdown-item">Padaria e Confeitaria</a><br/>
+                  <a href="dropdown-item">Para seu Animal</a><br/>
+                  <a href="dropdown-item">Produtos de Limpeza</a><br/>
+                  <a href="dropdown-item">Para sua Casa</a><br/>
                 </div>
               </li>
             </ul>
-              <form class="col-5">
-                  <input class="form-control" type="texte" placeholder="Buscar">
-                  <!-- <button class="input-group-addon" type="submit"><i class="fas fa-search"></i></button> -->
-              </form>
-            <ul class="nav">
-              <li class="navbar carrinho">
-                <a href="#" class="nav-link"><i class="fas fa-shopping-cart"></i></a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link login">Fazer Login</a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link cadastro">Cadastre-se</a>
-              </li>
-            </ul>
+            <form class="col-5 nav-item navi">
+              <input class="form-control" type="search" placeholder="Buscar">
+            </form>
+            <div class="nav-item">
+              <a href="#" class="carrinho"><i class="fas fa-shopping-cart"></i></a>
+            </div>
+            <div class="nav-item">
+              <button href="#" class="btn btn-outline-dark">Fazer Login</button>
+              <button href="#" class="btn cadastro">Cadastre-se</button>
+            </div>
         </nav>
       </header>
-        <div>
-          <div>
-            <div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col slide">
 
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col">
+            <h4 class="prom">Promoção</h4>
+          </div>
+        </div>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col prod">
+              Produto
+            </div>
+            <div class="col prod">
+              Produto
+            </div>
+            <div class="col prod">
+              Produto
+            </div>
+            <div class="col prod">
+              Produto
             </div>
           </div>
         </div>
+      </div>
     </main>
     <footer>
 
