985ce1894cb5de8d16e07dccef94cb86412a650f
Brenno <brennohenriqueskt@gmail.com>
2018-05-07 00:23:30 -0300

    Subindo arquivos

---
+++ index.php
@@ -0,0 +1,80 @@
+<html lang="pt-br">
+
+  <head>
+    <title>POTM</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="_css/style.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+  </head>
+
+  <body>
+
+    <main class="container-fluid principal">
+      <header class="row">
+        <nav class="navbar container-fluid">
+            <a href="#" class="navbar-brand">Logo</a>
+            <ul class="nav">
+              <li class="dropdown cat">
+                <a href="#" class="nav-link" id="linkDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-th"></i>Categorias
+                </a>
+                <div class="dropdown-menu cat2" aria-labelledby="linkDrop">
+                  <table>
+                    <tr>
+                      <td><a href="dropdown-item">Hortifruti</a></td><td><a href="dropdown-item">Açougue</a></td>
+                    </tr>
+                    <tr>
+                      <td><a href="dropdown-item">Frios e Laticínios</a></td><td><a href="dropdown-item">Adega</a></td>
+                    </tr>
+                    <tr>
+                      <td><a href="dropdown-item">Papelaria</a></td><td><a href="dropdown-item">Padaria e Confeitaria</a></td>
+                    </tr>
+                    <tr>
+                      <td><a href="dropdown-item">Produtos para seu Animal</a></td><td><a href="dropdown-item">Produtos de Limpeza</a></td>
+                    </tr>
+                    <tr>
+                      <td><a href="dropdown-item">Para sua Casa</a></td>
+                    </tr>
+                  </table>
+                </div>
+              </li>
+            </ul>
+              <form class="col-5">
+                  <input class="form-control" type="texte" placeholder="Buscar">
+                  <!-- <button class="input-group-addon" type="submit"><i class="fas fa-search"></i></button> -->
+              </form>
+            <ul class="nav">
+              <li class="navbar carrinho">
+                <a href="#" class="nav-link"><i class="fas fa-shopping-cart"></i></a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link login">Fazer Login</a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link cadastro">Cadastre-se</a>
+              </li>
+            </ul>
+        </nav>
+      </header>
+        <div>
+          <div>
+            <div>
+
+            </div>
+          </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+
+
+  </body>
+</html>
